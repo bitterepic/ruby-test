9994bb32931841a7ae20ae716cab2944df2751ad
Zachary Dovel <zachary@Slugger.local>
2026-03-26 17:15:04 +0900

    finish first database migration

--- db/migrate/20260325130715_initialization.rb
+++ db/migrate/20260325130715_initialization.rb
@@ -2,37 +2,49 @@
 
 # Initialization of the database
 class Initialization < ActiveRecord::Migration[8.1]
-  def change
+  def create_subscriptions_table
     create_table 'subscriptions', force: :cascade do
       t.string :user_id, null: false
-      t.datetime :expires_date, null: false
 
-      has_many :transactions
-      belongs_to :user, foreign_key: true
-      belongs_to :product, foreign_key: true
+      # Sort first
+      has_many :transactions, { order: 'purchase_date DESC' }
+      belongs_to :user, index: true, foreign_key: true
+      belongs_to :product, index: true, foreign_key: true
     end
+  end
 
+  def create_transactions_table
     create_table 'transactions', force: :cascade do
       t.string :notification_uuid, null: false
-      t.enum :status, [ :purchase, :renew, :cancel ]
+      t.enum :status, i[purchase, renew, cancel]
       t.decimal :amount, null: false
-      t.datetime :purchase_date, null: false
-      t.datetime :expires_date, null: false
-
-      belongs_to :subscription, foreign_key: true
+      t.datetime :purchase_date, null: false, index: true
+      t.datetime :expires_date, null: false, index: true
+      belongs_to :subscription, index: true, foreign_key: true
     end
+  end
 
+  def create_users_table
     create_table 'users', force: :cascade do
       t.string :given_name, null: false
       t.string :family_name, null: false
-      t.string :email, null: false
+      t.string :email, null: false, unique: true, index: true
 
-      has_many :transactions
-      has_many :subscriptions, through: :transactions
+      has_many :subscriptions
+      has_many :transactions, through: :subscriptions
     end
+  end
 
+  def create_products_table
     create_table 'product', force: :cascade do
       t.string :name, null: false
     end
+  end
+
+  def change
+    create_subscriptions_table
+    create_transactions_table
+    create_users_table
+    create_products_table
   end
 end
