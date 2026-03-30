d56004fccd444ad8afd63e5b474608c1086996c3
Zachary Dovel <zachary@Slugger.local>
2026-03-30 10:12:40 +0900

    more

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 # typed: strict
 
+
 class ApplicationController < ActionController::API
   before_action :authorized
 
   extend T::Sig
+
+  sig { returns(String) }
+  def hmac_secret
+    out = T.let(Rails.application.credentials[:hmac_secret], T.nilable(String))
+    throw "hmac_secret is unset in Rails Credentials.  Please add it" if out.nil?
+    out
+  end
 
   sig { void }
   def initialize
@@ -12,7 +20,7 @@ class ApplicationController < ActionController::API
 
   sig { params(payload: T.untyped).returns(String) }
   def encode_token(payload)
-    JWT.encode({data: payload, exp: Time.now.to_i + 4 * 3600 }, hmac_secret, 'HS256')
+    JWT.encode({ data: payload, exp: Time.now.to_i + 4 * 3600 }, hmac_secret, "HS256")
   end
 
   sig { returns(T.untyped) }
@@ -21,7 +29,7 @@ class ApplicationController < ActionController::API
     if header
       token = header.split(" ")[1]
       begin
-        JWT.decode(token, hmac_secret, true, { algorithm: 'HS256' }))
+        JWT.decode(token, hmac_secret, true, { algorithm: "HS256" })
       rescue JWT::DecodeError
         nil
       end
