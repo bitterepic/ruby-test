d3b50132604fab4b2f52e9c86c7bd907050f77df
Zachary Dovel <zachary@Slugger.local>
2026-04-05 23:11:35 +0900

    fix

--- app/controllers/v1/products_controller.rb
+++ app/controllers/v1/products_controller.rb
@@ -8,9 +8,10 @@ class V1::ProductsController < ApplicationController
   def index
     offset = params[:offset].nil? ? 0 : params[:offset].to_i || 0
     limit = params[:limit].nil? ? 20 : params[:limit].to_i
-    result = Product.all.offset(offset).limit(limit)
+    query = Product.all
+    result = query.offset(offset).limit(limit)
     products = T.let(result.to_a, T::Array[Product])
-    count = result.count
+    count = query.count
 
     render json: { products:, limit:, offset:, count: }
   end
--- app/controllers/v1/subscriptions_controller.rb
+++ app/controllers/v1/subscriptions_controller.rb
@@ -15,9 +15,10 @@ class V1::SubscriptionsController < ApplicationController
   def index
     offset = params[:offset].nil? ? 0 : params[:offset].to_i || 0
     limit = params[:limit].nil? ? 20 : params[:limit].to_i
-    result = Subscription.where(user_id: authenticated_user.id).limit(limit).offset(offset)
+    query = Subscription.where(user_id: authenticated_user.id)
+    result = query.limit(limit).offset(offset)
     subscriptions = T.let(result.to_a, T::Array[Subscription])
-    count = result.count
+    count = query.count
 
     render json: { subscriptions:, count:, offset:, limit: }
   end
